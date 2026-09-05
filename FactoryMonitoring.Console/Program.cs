9304cf260e1a90ca3d210a15c8e688d5699c5aff
Adam <adam.bartczak99@gmail.com>
2026-09-05 20:35:14 +0200

    Encapsulate Machine state

--- FactoryMonitoring.Console/Program.cs
+++ FactoryMonitoring.Console/Program.cs
@@ -15,8 +15,5 @@ machine.ChangeStatus(MachineStatus.Stopped);
 
 Console.WriteLine();
 Console.WriteLine(machine.Temperature);
-<<<<<<< HEAD
 Console.WriteLine(machine.Status);
-=======
-Console.WriteLine(machine.Status);
->>>>>>> feature/encapsulation
+
